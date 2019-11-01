679f14f4bb2d17b983ba7569973e7220f3c48701
conan25216 <conanjun@126.com>
2019-11-01 17:40:02 +0800

    add test data

--- routers/function.go
+++ routers/function.go
@@ -21,6 +21,6 @@ func GetTcfParas(ctx *macaron.Context) {
 	//para2 :=  strings.TrimSpace(ctx.Query("para2"))
 	//para3 :=  strings.TrimSpace(ctx.Query("para3"))
 	test_data := "52 200 10 167  1102 225 1 195 161 110 1 111 136 1"
-	stdout := processor.RunProcessor(test_data)
-	ctx.Resp.Write([]byte("response: " +stdout))
+	stdout,stderr := processor.RunProcessor(test_data)
+	ctx.Resp.Write([]byte("right output: " +stdout+"\nwrong output: "+stderr))
 }
